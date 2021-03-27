696098b00ead5b2e1d937e27f0beffd72a3dd19b
Sahibjeet Kaur <sahibjeetkaur1324@gmail.com>
2021-03-27 19:11:37 -0400

    authentication to profile

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/connectDB');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}));
 
 app.get('/', (req, res) => 
    res.json({msg: 'Welcome to the Contact Keeper API...'})
    );
 
-   app.use('/api/users', require('./routes/users'));
-   app.use('/api/auth', require('./routes/auth'));
+   app.use('/api/users', require('./routes/api/users'));
+   app.use('/api/auth', require('./routes/api/auth'));
+   app.use('/api/profile', require('./routes/api/profileRoutes'));
 
    const PORT = process.env.PORT || 5000;
    
-   app.listen(PORT, () => console.log('Server started on port'+' '+ PORT));
+   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
